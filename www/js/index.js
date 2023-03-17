d8ef924e019146fb45df9c6273f5a386817c68a6
Diego Lopez Ariza <diegoawesome2017@outlook.com>
2023-03-17 00:07:02 -0600

    proyect setup

---
+++ www/js/index.js
@@ -0,0 +1,29 @@
+const onLoad = () => {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+const onDeviceReady = () => {
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+    // pueden ser añadidos más eventos
+}
+
+const onPause = () => {
+    // Maneja el evento de la app llevada a segundo plano
+}
+
+const onResume = () => {
+    // Maneja el evento de la app traida a primer plano
+}
+
+const onMenuKeyDown = () => {
+    //  maneja el evento de botón menú
+}
+
+// listen for uncaught cordova callback errors
+window.addEventListener("cordovacallbackerror", event => {
+    // event.error contains the original error object
+})
+document.getElementById("regi").addEventListener("click", ()=> {location.href = "pages/register.html";})
+
---
+++ www/js/register.js
@@ -0,0 +1,5 @@
+const register = (e) => {
+    e.preventDefault()
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+}
