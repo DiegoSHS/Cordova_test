f35417239ad34c900fd875a0f1595ec05f655b8b
Diego Lopez Ariza <diegoawesome2017@outlook.com>
2023-03-17 22:23:15 -0600

    create blog fixed and estyles replaced

--- www/js/feed.js
+++ www/js/feed.js
@@ -5,29 +5,26 @@ const showBlogs = () => {
     const users = getUsers()
     const blogshtml = blogs.map(({ content, image, owner, }) => {
         const userOwner = users.find(u => u.id === owner)
-        const { user } = userOwner
+        return (
             `
         <div class="feed-items fitem">
             <div class="profiles">
-                <div class="profiles-img">
-                    <img src="../img/feed/users/user1.png" alt="">
-                </div>
+                <img class="profiles-img" src="../img/user.png" alt="">
                 <div class="profiles-text">
-                    ${owner}
+                    ${userOwner?.user}
                 </div>
             </div>
-            <div class="feed-item-img">
-                <div class="profiles-icon">
-                    <img src="../img/feed/icon _heart_.png">
-                </div>
-                <img src="${image}" alt="">
+            <img class="feed-item-img" src="${image}" alt="">
+            <div class="icon-heart">
+                <img class="profiles-icon" src="../img/feed/icon_heart_.png">
             </div>
-            <div>
+            <div class="blog-content">
                 ${content}
             </div>
         </div>`
+        )
     }).join('')
-    document.getElementById("feed").innerHTML = blogshtml
+    document.getElementById("blogs").innerHTML += blogshtml
 }
 //document.onload(showBlogs(), false)
 document.addEventListener("DOMContentLoaded", showBlogs, false)
