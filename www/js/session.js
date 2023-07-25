239555adc618ff689960107ea94248568e45c311
DiegoSHS <diegoawesome2017@outlook.com>
2023-07-25 16:53:13 -0600

    session and users modified (const keyword reduced)

--- www/js/session.js
+++ www/js/session.js
@@ -1,10 +1,10 @@
 export const getSession = () => {
     const session = JSON.parse(localStorage.getItem('session'))
     return session ? session : false
-}
-export const setSession = (session) => {
-    localStorage.setItem('session', JSON.stringify(session))
-}
-export const deleteSession = () => {
-    localStorage.removeItem('session')
-}
+},
+    setSession = (session) => {
+        localStorage.setItem('session', JSON.stringify(session))
+    },
+    deleteSession = () => {
+        localStorage.removeItem('session')
+    }
--- www/js/users.js
+++ www/js/users.js
@@ -1,45 +1,47 @@
-export const getUsers = () => {
+const getUsers = () => {
     const users = localStorage.getItem('users')
     return users ? JSON.parse(users) : []
-}
-export const setUsers = (users) => {
-    localStorage.setItem('users', JSON.stringify(users))
-}
-export const addUser = (id,{user,pass,email}) => {
-    const curruser = {id,user,pass,email}
-    const users = getUsers()
-    users.push(curruser)
-    setUsers(users)
-}
-export const getUser = (id) => {
-    const users = getUsers()
-    return users.find(u => u.id === id)
-}
-const removeUser = (user) => {
-    const users = getUsers()
-    const newUsers = users.filter(u => u.user !== user)
-    setUsers(newUsers)
-}
-const updateUser = (user, newUser) => {
-    const users = getUsers()
-    const newUsers = users.map(u => u.user === user ? newUser : u)
-    setUsers(newUsers)
-}
-const getPass = (pass) => {
-    const users = getUsers()
-    return users.find(u => u.pass === pass)
-}
-const updatePass = (pass, newPass) => {
-    const users = getUsers()
-    const newUsers = users.map(u => u.pass === pass ? newPass : u)
-    setUsers(newUsers)
-}
-const getMail = (mail) => {
-    const users = getUsers()
-    return users.find(u => u.mail === mail)
-}
-const updateMail = (mail, newMail) => {
-    const users = getUsers()
-    const newUsers = users.map(u => u.mail === mail ? newMail : u)
-    setUsers(newUsers)
-}
+},
+    setUsers = (users) => {
+        localStorage.setItem('users', JSON.stringify(users))
+    },
+    addUser = (id, { user, pass, email }) => {
+        const curruser = { id, user, pass, email }
+        const users = getUsers()
+        users.push(curruser)
+        setUsers(users)
+    },
+    getUser = (id) => {
+        const users = getUsers()
+        return users.find(u => u.id === id)
+    },
+    removeUser = (user) => {
+        const users = getUsers()
+        const newUsers = users.filter(u => u.user !== user)
+        setUsers(newUsers)
+    },
+    updateUser = (user, newUser) => {
+        const users = getUsers()
+        const newUsers = users.map(u => u.user === user ? newUser : u)
+        setUsers(newUsers)
+    },
+    getPass = (pass) => {
+        const users = getUsers()
+        return users.find(u => u.pass === pass)
+    },
+    updatePass = (pass, newPass) => {
+        const users = getUsers()
+        const newUsers = users.map(u => u.pass === pass ? newPass : u)
+        setUsers(newUsers)
+    },
+    getMail = (mail) => {
+        const users = getUsers()
+        return users.find(u => u.mail === mail)
+    },
+    updateMail = (mail, newMail) => {
+        const users = getUsers()
+        const newUsers = users.map(u => u.mail === mail ? newMail : u)
+        setUsers(newUsers)
+    }
+
+export { getUsers, setUsers, getUser, addUser }
